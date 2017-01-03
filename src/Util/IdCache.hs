a0730bca60d00610dd928e8d7486778857bea710
tomjefferys <tomjefferys@gmail.com>
2017-01-03 21:48:19 +0000

    Ensure the IdCache uses ids in the freeIds set if they are available

--- src/Util/IdCache.hs
+++ src/Util/IdCache.hs
@@ -6,9 +6,7 @@ import Data.IntSet (IntSet)
 import qualified Data.IntSet as IntSet
 import Data.Maybe (fromMaybe)
 
--- TODO rename manager -> cache ?
-
--- | Manager type.
+-- | Cache type.
 -- Holds the current map, and the next id
 data IdCache v =
     IdCache {
@@ -19,8 +17,8 @@ data IdCache v =
     }
 
 member :: Int -> IdCache v -> Bool
-member n manager =
-    IntMap.member n $ getMap manager
+member n cache =
+    IntMap.member n $ getMap cache
 
 -- | Makes an empty id cache
 empty :: String -- ^ A name, used for error reporting
@@ -29,49 +27,61 @@ empty name = IdCache name IntMap.empty 0 IntSet.empty
 
 -- | Get all the values along with their ids
 allValues :: IdCache v -> [(Int, v)]
-allValues manager = IntMap.toList $ getMap manager
+allValues cache = IntMap.toList $ getMap cache
 
 allIds :: IdCache v -> [Int]
 allIds cache = IntMap.keys $ getMap cache
 
--- | Adds an value to the manager
+-- | Adds an value to the cache
 -- returns a tuple containing the id assigned to the 
 -- new value, and the updated IdManger
 addValue :: v -> IdCache v -> (Int, IdCache v)
-addValue value manager =
-    let idMap = getMap manager
-        nextId = getNextId manager
+addValue value cache =
+    let (nextId, cache') = getNextAvailableId cache
+        idMap = getMap cache'
     in (nextId,
-         manager { getMap = IntMap.insert nextId value idMap,
-                   getNextId = nextId + 1 })
+         cache' { getMap = IntMap.insert nextId value idMap})
+
+-- | get the next available id, either from the freeIds set,
+-- or from the nextId field
+getNextAvailableId :: IdCache v -> (Int, IdCache v)
+getNextAvailableId cache = 
+  if IntSet.null $ getFreeIds cache
+    then 
+      let nextId = getNextId cache
+      in (nextId, cache { getNextId = nextId + 1})
+    else
+      let (nextId, freeIds) = IntSet.deleteFindMin
+                                $ getFreeIds cache
+      in (nextId, cache {getFreeIds = freeIds})
 
 -- | Gets a value from it's id
 -- errors if the id does not exist
 getValue :: Int -> IdCache v -> v
-getValue valId manager = 
-    let idMap = getMap manager
+getValue valId cache = 
+    let idMap = getMap cache
         mVal = IntMap.lookup valId idMap
     in fromMaybe (error 
-            (getName manager
+            (getName cache
               ++ " getValue: No such Id: "
               ++ show valId))
             mVal
 
 -- | Similar to Map.lookup
 lookup :: Int -> IdCache v -> Maybe v
-lookup valId manager =
-  let idMap = getMap manager
+lookup valId cache =
+  let idMap = getMap cache
   in IntMap.lookup valId idMap
 
     
 
 -- | Updates a value, errors if id does not already exist
 updateValue :: Int -> v -> IdCache v -> IdCache v
-updateValue valId val manager =
-    let idMap = getMap manager
+updateValue valId val cache =
+    let idMap = getMap cache
     in if IntMap.member valId idMap
-        then manager { getMap = IntMap.insert valId val idMap }
-        else error (getName manager
+        then cache { getMap = IntMap.insert valId val idMap }
+        else error (getName cache
                      ++ "updateValue: No such id: "
                      ++ show valId)
 
